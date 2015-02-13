6380363523daf9dd8275dbd8803c7ca7838e8446
jlgranda <jlgranda@dev.jlgranda.com>
2015-02-12 20:05:59 -0500

    optimizaciones

--- src/main/java/org/jpapi/util/Lists.java
+++ src/main/java/org/jpapi/util/Lists.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2012 jlgranda.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS\" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jpapi.util;
+package net.tecnopro.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,31 +10,18 @@ import java.util.List;
  * @author jlgranda
  */
 public class Lists {
-
     /**
      * Convierte una lista de objetos en una cadena separada por comas, con los
      * elementos de la lista. Los objetos de la lista deben implementar el
      * método toString
      *
-     * @param list la lista sobre la cual se construye la cadena de elementos
-     * seperadas por comas
+     * @param list
+     *            la lista sobre la cual se construye la cadena de elementos
+     *            seperadas por comas
      * @return una cadena separada por comas, con los elementos de la lista.
      */
     public static String listToString(List list) {
-
-        if (list.isEmpty()) {
-            return "-";
-        }
-
-        StringBuilder buffer = new StringBuilder();
-        int index = 0;
-        for (Iterator _iterator = list.iterator(); _iterator.hasNext(); buffer.append(_iterator.next().toString())) {
-            if (index > 0) {
-                buffer.append(", ");
-            }
-            index++;
-        }
-        return buffer.toString();
+        return Lists.listToString(list, ',', (char) Character.SPACE_SEPARATOR);
     }
 
     /**
@@ -57,18 +29,16 @@ public class Lists {
      * elementos de la lista. Los objetos de la lista deben implementar el
      * método toString
      *
-     * @param list la lista sobre la cual se construye la cadena de elementos
-     * seperadas por comas
+     * @param list
+     *            la lista sobre la cual se construye la cadena de elementos
+     *            seperadas por comas
      * @return una cadena separada por comas, con los elementos de la lista.
      */
     public static String listToString(String[] list) {
-
         if (list == null) {
             return "-";
         }
-
         StringBuilder buffer = new StringBuilder();
-
         int length = list.length;
         for (int i = 0; i < length; i++) {
             buffer.append(list[i]);
@@ -84,20 +54,42 @@ public class Lists {
      * los elementos de la lista. Los objetos de la lista deben implementar el
      * método toString
      *
-     * @param list la lista sobre la cual se construye la cadena de elementos
-     * seperadas por comas
+     * @param list
+     *            la lista sobre la cual se construye la cadena de elementos
+     *            seperadas por comas
      * @return una cadena separada por separador, con los elementos de la lista.
      */
     public static String listToString(List list, char separador) {
-
+        return Lists.listToString(list, separador, '\0');
+    }
+   
+    /**
+     * Convierte una lista de objetos en una cadena separada por separador, con
+     * los elementos de la lista. Los objetos de la lista deben implementar el
+     * método toString
+     *
+     * @param list
+     *            la lista sobre la cual se construye la cadena de elementos
+     *            seperadas por comas
+     * @return una cadena separada por separador, con los elementos de la lista.
+     */
+    public static String listToString(List list, char separador, char delimitador) {
         if (list.isEmpty()) {
             return "-";
         }
-
         StringBuilder buffer = new StringBuilder();
         int index = 0;
-        for (Iterator _iterator = list.iterator(); _iterator.hasNext(); buffer.append(_iterator.next().toString())) {
-            if (index > 0) {
+        Object value = null;
+        String str;
+        for (Iterator _iterator = list.iterator(); _iterator.hasNext(); ) {
+            value = _iterator.next();
+            str = (value == null ? "" : value.toString());
+            str = str.replaceAll("\'", "\\\'");
+            buffer.append(delimitador)
+            .append(str)
+            .append(delimitador);
+           
+            if (index < (list.size() - 1)) {
                 buffer.append(separador);
                 buffer.append(' ');
             }
@@ -111,19 +103,19 @@ public class Lists {
      * los elementos de la lista. Los objetos de la lista deben implementar el
      * método toString
      *
-     * @param list la lista sobre la cual se construye la cadena de elementos
-     * seperadas por comas
+     * @param list
+     *            la lista sobre la cual se construye la cadena de elementos
+     *            seperadas por comas
      * @return una cadena separada por separador, con los elementos de la lista.
      */
     public static String listToString(List list, String separador) {
-
         if (list.isEmpty()) {
             return "-";
         }
-
         StringBuilder buffer = new StringBuilder();
         int index = 0;
-        for (Iterator _iterator = list.iterator(); _iterator.hasNext(); buffer.append(_iterator.next().toString())) {
+        for (Iterator _iterator = list.iterator(); _iterator.hasNext(); buffer
+                .append(_iterator.next().toString())) {
             if (index > 0) {
                 buffer.append(separador);
             }
@@ -138,15 +130,18 @@ public class Lists {
         }
         return str.toUpperCase();
     }
+
     private static final int MINIMUM_SUPPORTED_LENGTH = 4;
 
     /**
      * Truncate text on a whitespace boundary (near a specified length). The
      * length of the resultant string will be in the range:<br>
-     * <code>   (requested-length * .25) ~ (requested-length * 1.5) </code>
+     * <code> (requested-length * .25) ~ (requested-length * 1.5) </code>
      *
-     * @param text Text to truncate
-     * @param length Target length
+     * @param text
+     *            Text to truncate
+     * @param length
+     *            Target length
      * @return Truncated text
      */
     public static String truncateAtWhitespace(String text, int length) {
@@ -155,8 +150,9 @@ public class Lists {
          * Make sure we have a reasonable length to work with
          */
         if (length < MINIMUM_SUPPORTED_LENGTH) {
-            throw new IllegalArgumentException("Requested length too short (must be "
-                    + MINIMUM_SUPPORTED_LENGTH + " or greated)");
+            throw new IllegalArgumentException(
+                    "Requested length too short (must be "
+                            + MINIMUM_SUPPORTED_LENGTH + " or greated)");
         }
         /*
          * No need to truncate - the original string "fits"
@@ -169,7 +165,6 @@ public class Lists {
          */
         lowerBound = length / 4;
         upperBound = length + (length / 2);
-
         for (int i = length - 1; i > lowerBound; i--) {
             if (Character.isWhitespace(text.charAt(i))) {
                 return text.substring(0, i);
@@ -190,21 +185,19 @@ public class Lists {
     }
 
     public static List<String> stringToList(String s) {
-
         if (s == null) {
             return new ArrayList<String>();
         }
-
         if (s.isEmpty()) {
             return new ArrayList<String>();
         }
         return Arrays.asList(s.split(","));
     }
-    
+
     public static String findDefaultValue(String values) {
         return findDefaultValue(values, "*");
     }
-    
+
     public static String findDefaultValue(String values, String mnemotecnico) {
         List<String> options = Lists.stringToList(values);
         String defaultValue = null;
@@ -214,5 +207,19 @@ public class Lists {
             }
         }
         return defaultValue;
+    }
+   
+    public static void main(String args[]){
+        String scape = "ORD-CHICAGO-O'HARE INTERNATIONAL AIRPORT".replace("\'", "\\\'");
+        List<String> tests = new ArrayList<String>();
+        tests.add(scape);
+        tests.add(scape);
+        tests.add(scape);
+        tests.add(scape);
+        tests.add(scape);
+        tests.add(scape);
+       
+       
+        System.out.print(Lists.listToString(tests, ',', '\''));
     }
 }
